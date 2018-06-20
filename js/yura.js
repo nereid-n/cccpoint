9a0146095b3e05ca54c96e146c0acd89922b41a0
Nereid <nas.kuleshova@gmail.com>
2018-06-20 12:26:49 +0300

    second commit

--- js/yura.js
+++
@@ -1,4 +0,0 @@
-$(document).on('click', '.crypto-header__mobile-btn', function () {
-  $(this).toggleClass('crypto-header__mobile-btn-cross');
-  $(this).siblings('.crypto-header__mobile-menu').slideToggle('fast');
-});
